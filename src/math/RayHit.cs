5ec9783286cd68fa8a0742ebd85a71928fb628b6
jayden-woo <woojw@student.unimelb.edu.au>
2021-08-23 00:50:01 +1000

    docs: add documentation for class and method in RayHit.cs
    * add the missing documentation and properties in the RayHit class

--- src/math/RayHit.cs
+++ src/math/RayHit.cs
@@ -11,21 +11,44 @@ namespace RayTracer
         private Vector3 position;
         private Vector3 normal;
         private Vector3 incident;
+        private Material material;
 
+        /// <summary>
+        /// Construct a new ray hit data.
+        /// </summary>
+        /// <param name="position">The position of the intersection point</param>
+        /// <param name="normal">The normal of the intersected surface</param>
+        /// <param name="incident">The direction of the incident ray</param>
+        /// <param name="material">The material of the intersected surface</param>
         public RayHit(Vector3 position, Vector3 normal, Vector3 incident, Material material)
         {
             this.position = position;
             this.normal = normal;
             this.incident = incident;
+            this.material = material;
         }
 
-        // You may wish to write methods to compute other vectors, 
+        // You may wish to write methods to compute other vectors,
         // e.g. reflection, transmission, etc
 
+        /// <summary>
+        /// The position of the intersection point.
+        /// </summary>
         public Vector3 Position { get { return this.position; } }
 
+        /// <summary>
+        /// The normal of the intersected surface.
+        /// </summary>
         public Vector3 Normal { get { return this.normal; } }
 
+        /// <summary>
+        /// The direction of the incident ray.
+        /// </summary>
         public Vector3 Incident { get { return this.incident; } }
+
+        /// <summary>
+        /// The material of the intersected surface.
+        /// </summary>
+        public Material Material { get { return this.material; } }
     }
 }
